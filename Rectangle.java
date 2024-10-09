24e49cc167888a04e4b5738359c287dc42dca0ac
pranav2748 <perukulapati01@bvsd.org>
2024-10-09 21:51:20 +0000

    done

--- Rectangle.java
+++ Rectangle.java
@@ -6,14 +6,14 @@ public class Rectangle
     private int width;
 
 
-    public Retangle (xPosition, yPosition, length, width)
+    public Rectangle (int xPosition, int yPosition, int length, int width)
     {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this.length = length;
         this.width = width;
     }
-    public Rectangle(length, width)
+    public Rectangle(int length, int width)
     {
         this(0, 0, length, width);
     }
@@ -42,29 +42,32 @@ public class Rectangle
     {
         if (this.length == this.width)
         {
-            return true
+            return true;
         }
-        if (this.length != this.width)
+        else
         {
-            return false
+            return false;
         }
     }
     public boolean equals(Rectangle other)
     {
         if (this.xPosition == other.xPosition && this.yPosition == other.yPosition && this.width == other.width && this.length == other.length)
         {
-            return true
+            return true;
         }
         else
         {
-            return false
+            return false;
         }
     }
     public static void main(String[] args)
     {
         Rectangle r1 = new Rectangle(5,4);
         Rectangle r2 = new Rectangle(5,4);
-        system.out.println(r1.equals(r2));
+        Rectangle r3 = new Rectangle(6,4);
+        System.out.println(r1.equals(r3));
+        System.out.println(r2.isSquare);
+       
     }
 
 
